26e7d4e9bb265c98facb21f992750f0fdde9eb1a
lia219 <lia219@lehigh.edu>
2016-03-09 01:02:06 +0000

    updated TextSpinner

--- hw06/Fibonacci.java
+++ hw06/Fibonacci.java
@@ -3,27 +3,30 @@
 // Fibonacci numbers
 // 3/8/16
 
-import java.util.Scanner; // setting up import scanner
-public class Fibonacci { // start of class
- 
-    public static void main(String [] args){ // start of main method 
-    Scanner input = new Scanner (System.in); // scanner input set up 
-    System.out.println( "Enter the first number in the sequence: ");
-    int fib = myScanner.nextint ();
-    System.out.println( "Enter the second number in the sequence: ");
-    int fib = myScanner.nextint ();
-    System.out.println("How many custom Fibonacci numbers should be printed: ");
- 
-         int fibCount = ; 
-         int[] fib = new int[fibCount]; 
-         fib[0] = 0;
-         fib[1] = 1;
-         for(int n=2; n < febCount; n++){ // beginning of for loop and equation for calculating a Fibonacci number 
-             fib[n] = fib[n-1] + fib[n-2];
-         } // end of for loop 
- 
-         for(int n=0; n< febCount; n++){ // beginning of second for loop for the amount of numbers in the sequence 
-                 System.out.print(("The numbers are: ") fib[n] + " "); // printing out the results 
-         } // end of second for loop 
-    } // end of main method 
-} // end of class 
+import java.util.Scanner; // import scanner set up
+public class Fibonacci{ // start of class
+    public static void main(String args[]){ // start of main method
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter the first number in the sequence: "); // telling user to enter the first integer that will be in the fibonacci sequence
+        int num1 = myScanner.nextInt(); // setting up scanner for the first number
+        System.out.println("Enter the second number in the sequence: "); // telling the user to enter the second integer in the fibonacci
+        int num2 = myScanner.nextInt(); // setting up scanner for the second number
+        System.out.println("How many custom Fibonacci numbers should be printed?: ");
+        int sequenceNum = myScanner.nextInt(); // telling user to enter the amount of fibonacci numbers to put int he sequence
+        while( num1 < 0 || num2 < 0 || sequenceNum < 0) { // setting up while loop stating that both numbers must be re entered if not positive
+        System.out.println("Integers entered are not valid"); // stating that integers are invalid if not positive
+        System.out.println("Please enter the first positive integer again: ");// asking user to re enter first integer if not valid the first time
+        num1 = myScanner.nextInt();
+        num2 = myScanner.nextInt();
+        sequenceNum = myScanner.nextInt();
+        } // end of while loop
+        int fibNum = ((((sequenceNum) - (num1)) + ((sequenceNum) - (num2))));
+        System.out.println("The numbers are: " + fibNum); // final print out of fibonacci sequence
+    } // end of main method
+} //end of class
+        
+        
+        
+        
+        
+        
---
+++ hw06/TextSpinner.java
@@ -0,0 +1,20 @@
+// Lindsay Alvarez
+// hw06
+// Text Spinner program using infinite loop
+// 3/8/16
+public class TextSpinner {  // start of Text Spinner class
+    public static void main (String [ ] args){ // start of main method 
+        
+   
+
+
+
+while(true){ // while loop that says if the statment is true, it will print each part of the spinner, then delete it and re print it forever
+            System.out.print("/\b");  // first part of spinner
+            System.out.print("-\b"); // second symbol in spinner
+            System.out.print("\\\b"); // third symobol in spinner
+            System.out.print("|\b"); // fourth symbol in spinner
+            
+}// end of while loop
+}// end of main method 
+}//end of class
