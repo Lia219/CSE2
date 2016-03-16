e75ea6e0048fa9a14fd4951c63ba7e5f13b28ad2
lia219 <lia219@lehigh.edu>
2016-03-16 01:43:56 +0000

    updated Triangle

---
+++ hw07/Triangle.java
@@ -0,0 +1,106 @@
+//Lindsay Alvarez
+//hw07
+//3/15/16
+// Twisty java 
+// Program will take an input between 5 and 30 and create a triangle out of it that has the same amount of rows as the input number
+
+import java.util.Scanner;
+public class Triangle { // start of class
+                 public static void main(String[] args) { // start of main method
+ Scanner myScanner;    // import scanner
+myScanner = new Scanner( System.in );
+System.out.print("Please enter an integer between 5 and 30: ");   // ask user for a value between 5 an 30
+int i =1;
+while(i<2){   // run a while loop to so that a correct value is entered
+i++;
+    if (myScanner.hasNextInt()){  // make sure input is an integer not a letter or negative number 
+        int number= myScanner.nextInt();
+        if (number>=5 && number<=30){  // if it is an integer and it falls between the accepted range the loop will run
+            System.out.println("FOR LOOP:"); 
+            for( int Count = 1; Count<=number; Count++ ) { // set up a loop to run the same number of times as the integer entered
+                if(Count%2==0){  // test if it is printing an even or odd number
+                    for( int Repeat = 1; Repeat<=Count; Repeat++ ) {  //set up a loop to break the line every time it is run when the triangle is finished
+                        for( int Stack  = 1; Stack<=Repeat; Stack++ ) {   //Set up a loop to print the number in an rising triangle structure 
+                        } 
+                    System.out.println(" ");    
+                    }    
+                } 
+                else{ // test if the triangle is printing an even or an odd number
+                    for( int Repeat = 1; Repeat<=Count; Repeat++ ) { //set up a loop to break the line every time it is run
+                        for( int Stack = Count; Stack>=Repeat; Stack-- ) {   //Set up a loop to print the number in an decreasing triangle structure 
+                        } 
+                    System.out.println(" ");    
+                    }       
+                }
+            } 
+            System.out.println("WHILE LOOP:");
+            int Count = 1;
+            while(Count<=number  ) {// set up a loop to run the same number of times as the number value
+                if(Count%2==0){ // test if the triangle is printing an even or an odd number
+                    int Repeat = 1;
+                    while(  Repeat<=Count  ) {//set up a loop to break the line every time it is run
+                        int Stack = 1;
+                        while(  Stack<=Repeat  ) { //Set up a loop to print the number in an increasing triangle structure
+                        Stack++;
+                        } 
+                    System.out.println(" ");
+                    Repeat++;
+                    }    
+                } 
+                else{ // test if the current cycle is printing an even or an odd number
+                    int Repeat = 1;
+                    while(  Repeat<=Count  ) {//set up a loop to break the line every time it is run
+                        int Stack = Count;
+                        while(  Stack>=Repeat  ) {//Set up a loop to print the number in an decreasing triangle structure strting with it printing the number the same amount of times as its value
+                        System.out.print(Count);
+                        Stack--;
+                        } 
+                    System.out.println(" ");   
+                    Repeat++;
+                    }       
+                }
+                Count++;
+            }
+             System.out.println("DO WHILE LOOP:");
+             Count = 1;
+            do {   // set up a loop to run the number of times as the iputeed value
+                if(Count%2==0){ // test if the current cycle is printing an even or an odd number
+                    int Repeat = 1;
+                   do { //set up a loop to break the line every time it is run
+                        int Stack = 1;
+                       do {  //Set up a loop to print the number in an increasing triangle structure until it print the number the same amount of times as its value
+                        System.out.print(Count);
+                        Stack++;
+                        }  while(  Stack<=Repeat  );
+                    System.out.println(" ");
+                        Repeat++;
+                    } while(  Repeat<=Count  )  ;  
+                } 
+                else{ // test if the current cycle is printing an even or an odd number
+                    int Repeat = 1;
+                    do { //set up a loop to break the line every time it is run
+                        int Stack = Count;
+                        do { //Set up a loop to print the number in an decreasing triangle structure strting with it printing the number the same amount of times as its value
+                        System.out.print(Count); 
+                        Stack--;
+                        } while(  Stack>Repeat  );
+                    System.out.println(" ");   
+                    Repeat++;
+                    }while(  Repeat<=Count  ) ; // set up a loop to run the number of times as the number value
+                }
+                Count++;
+            }while(Count<=number  );
+        }
+        else{ // else loop for entering a number that is not betweeen 5 and 30
+            System.out.println("Pleases enter an integer between 5 and 30, Please try again"); // print out asking for integer that is positive and between 5 and 30
+            i--;
+        }
+    }
+    else{
+            System.out.println("Please enter an integer between 5 and 30, Please try again"); // final print out asking for integer if first integer was not positive or between 5 an 30
+               i--;
+               myScanner.next();
+        }
+} // end of class
+}} // end of main method
+ 
--- lab06/TwistGenerator.java
+++ lab06/TwistGenerator.java
@@ -7,16 +7,88 @@ import java.util.Scanner;
 public class TwistGenerator { // start of class
 public static void main (String [ ] args) { // start of main method
 Scanner input = new Scanner (System.in); // scanner
-int length = input.nextInt();// 
-while ( length > 0 ) { // beginning of loop container 
-break; // stop in the loop 
-} // end of loop container 
+int length = 0;// setting the integer length to 0 
+  System.out.print("Enter the desired length of the twist: "); // asking the user to input a length for the twist 
+        while(true) {
+        if (input.hasNextInt()) { // while loop for entering the length into the scanner 
+            length = input.nextInt();
+            if(length<0) { continue;
+            }
+            break;
+        } // end of while loop 
+        //if ( length < 0 )// if statement for the user putting in a negative integer
+   System.out.println("invalid input, enter again");//if it is a negative input, ask the user to enter another number
+   input.next();
+        //} // end of if statement
+   //else{ // beginning of the else statement that states if the input is positive, continue the loop
+    //break;
+   //} // end of else statement 
+        }
+   
+  System.out.println("The length of your twist is: " + length); // telling the user what the length of the twist will be 
+        int fullTwist = (length / 3); // the formula for a full twist 
+        int partofTwist = (length % 3); // the formula for only part of a twist 
+ int a = 1; 
+        
+        while(a <= fullTwist){ // while loop for the int a being a full twist 
+        System.out.print("\\ /"); // the symbols for the top line of the twist 
+        a++; // adding 1 to int a 
+        } // end of while loop 
+        
+        if(partofTwist == 0){ // if statement that if part of the twist is 0 than only the one symbol will be printed
+            System.out.printf("\n"); // the symbol that will be printed from the if statementif the statement is true
+            a = 1; 
+        } // end of if statement 
+        else if(partofTwist == 1){ // beginning of else if statement for part of the twist equaling 1
+            System.out.print("\\"); // this will be printed if the input is 1
+            System.out.printf("\n"); 
+            a = 1;
+        }  // end of else if 
+        else if (partofTwist == 2){ // if the input is 2 then the following will be printed
+            System.out.print("\\ ");
+            System.out.printf("\n");
+            a = 1;
+        } // end of else if
+         while(a <= fullTwist){
+        System.out.print(" X ");
+        a++;
+        }
+        
+        if(partofTwist == 0){
+            System.out.printf("\n");
+            a = 1;
+        }
+        else if(partofTwist == 1){
+            System.out.print(" ");
+            System.out.printf("\n");
+            a = 1;
+        } 
+        else if (partofTwist == 2){
+            System.out.print(" X");
+            System.out.printf("\n");
+            a = 1;
+        }
+        
+        while(a <= fullTwist) {
+        System.out.print("/ \\");
+        a++;
+        }
+        
+       if(partofTwist == 0){
+            System.out.printf("\n");
+            a = 1;
+        }
+        else if(partofTwist == 1){
+            System.out.print("/");
+            System.out.printf("\n");
+            a = 1;
+        } 
+        else if (partofTwist == 2){
+            System.out.print("/ \\");
+            System.out.printf("\n");
+            a = 1;
+        }
 
-System.out.println("\\/") ; // printing out the top line of a twist
-
-System.out.println("X"); // printing out the center line of a twist 
-
-System.out.println("/\\"); // printing out the bottom line of a twist 
 
 } // end of main method
     
